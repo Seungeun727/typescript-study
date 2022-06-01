4f5601c02fd9846d1921b1cfd854bdd1a01260e3
seun2230 <seun2230@gmail.com>
2022-06-01 12:59:44 +0900

    Type: Array, Tuple

---
+++ type/tuple.ts
@@ -0,0 +1,19 @@
+// Tuple
+// 다른 Type, 같은 Type을 대괄호로 표현함
+// 단 타입 유형, 순서도 같아야 함.
+let x: [ string, number ];
+
+x = [ "hello", 39 ];
+// x = [ 39, "hello" ];   // error 
+
+// 인덱스 표현 가능함.
+// 인덱스의 해당 요소가 없으면 undefind로 지정된다.
+
+// x[3] = "world";  // error
+
+const person: [ string, number ] = [ "Mark", 39 ];
+
+// 분해 할당
+const [] = person;
+
+// const [ first, second, third ] = person; // first: string, second : number
