9edd962a01a40f86428a77e435c22f6f2693bcc2
seun2230 <seun2230@gmail.com>
2022-06-13 21:57:13 +0900

    Variable: Property rename, spread

--- variable/variable.js
+++ variable/variable.js
@@ -1,5 +1,16 @@
 // Typescript Gitbook 참고
 // 배열 구조 분해
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -10,6 +21,15 @@ var __rest = (this && this.__rest) || function (s, e) {
                 t[p[i]] = s[p[i]];
         }
     return t;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 function exampleFunction(_a) {
     var first = _a[0], second = _a[1];
@@ -40,6 +60,35 @@ var product = {
     price: 10,
     inventory: 5
 };
-var item = product.item, remain = __rest(product, ["item"]);
-var productTotal = remain.price + remain.inventory;
-console.log("productTotal", productTotal);
+// let { item, ...remain} = product;
+// let productTotal = remain.price + remain.inventory;
+// console.log("productTotal", productTotal);
+// 프로퍼티 rename
+// let { item: newName1, price: newName2 } = product;
+// let newName1 = product.item;
+// let newName2 = product.price;
+var item = product.item, price = product.price;
+// 전개
+// 배열을 다른 배열 안에 전개할 때 사용함
+// 배열을 이용한 전개
+// 전개로 인해 값이 변하지 않음
+var first = [1, 2];
+var second = [3, 4];
+var bothPlus = __spreadArray(__spreadArray(__spreadArray([0], first, true), second, true), [5], false);
+console.log("bothPlus", bothPlus); // result : [0, 1, 2, 3, 4, 5]
+// 객체를 이용한 전개
+// 전개 객체에 마지막에 작성한 프로퍼티가 이전에 프로퍼티를 덮어쓰기한다.
+var defualts = {
+    food: "spicy",
+    price: "$$",
+    ambiance: "noisy"
+};
+var search = __assign(__assign({}, defualts), { food: "rich" });
+console.log("search", search); // result : { food: 'rich', price: '$$', ambiance: 'noisy' }
+var store = {
+    staff: "steve",
+    product: "food",
+    invenory: 5
+};
+var modifyStore = __assign(__assign({}, store), { staff: "jane" });
+console.log("modifyStore", modifyStore);
--- variable/variable.ts
+++ variable/variable.ts
@@ -41,7 +41,55 @@ let product = {
 };
 
 
-let { item, ...remain} = product;
-let productTotal = remain.price + remain.inventory;
+// let { item, ...remain} = product;
+// let productTotal = remain.price + remain.inventory;
 
-console.log("productTotal", productTotal);
+// console.log("productTotal", productTotal);
+
+
+// 프로퍼티 rename
+// let { item: newName1, price: newName2 } = product;
+// let newName1 = product.item;
+// let newName2 = product.price;
+
+let { item, price }: {item: string, price: number} = product;
+
+// 전개
+// 배열을 다른 배열 안에 전개할 때 사용함
+
+
+// 배열을 이용한 전개
+// 전개로 인해 값이 변하지 않음
+let first = [1, 2];
+let second = [3, 4];
+let bothPlus = [0, ...first, ...second, 5];
+console.log("bothPlus", bothPlus);    // result : [0, 1, 2, 3, 4, 5]
+
+// 객체를 이용한 전개
+// 전개 객체에 마지막에 작성한 프로퍼티가 이전에 프로퍼티를 덮어쓰기한다.
+let defualts = {
+  food: "spicy",
+  price: "$$",
+  ambiance: "noisy"
+};
+
+let search = {
+  ...defualts,
+  food: "rich"
+};
+
+console.log("search", search);  // result : { food: 'rich', price: '$$', ambiance: 'noisy' }
+
+
+let store = {
+  staff: "steve",
+  product: "food",
+  invenory: 5
+};
+
+let modifyStore = {
+  ...store,
+  staff: "jane"
+};
+
+console.log("modifyStore", modifyStore);
